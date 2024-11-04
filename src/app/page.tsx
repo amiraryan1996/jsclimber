7b5f990accb8220d8762e2794bc394aed6358125
amiraryan1996 <ariyaan021@gmail.com>
2024-11-05 00:19:18 +0330

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function page() {
   return (
-    <div className="h-[50vh] bg-black text-white">
+    <div className="h-[50vh] bg-red-800 text-white">
       home page hello cpanel again! let me do my try!! page page tailwind added to dep
       <div className="bg-red-200">asghar help me! i test it</div>
       <div>i will come back in 1 hour bye! i need sleep feeling asleepy</div>
