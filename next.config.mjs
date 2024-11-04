7d2336bbb23fbc807f6ec53f7851b9aad1d06442
amiraryan1996 <ariyaan021@gmail.com>
2024-11-04 23:37:36 +0330

    test

--- next.config.mjs
+++ next.config.mjs
@@ -8,36 +8,26 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
-    config.cache = false; // Disable caching as needed
-
-    // ! https://react-svgr.com/docs/next/
-    // Grab the existing rule that handles SVG imports
+    config.cache = false;
     const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
-
     config.module.rules.push(
-      // Reapply the existing rule, but only for svg imports ending in ?url
       {
         ...fileLoaderRule,
         test: /\.svg$/i,
         resourceQuery: /url/, // *.svg?url
       },
-      // Convert all other *.svg imports to React components
       {
         test: /\.svg$/i,
         issuer: fileLoaderRule.issuer,
-        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
         use: ['@svgr/webpack'],
       },
     );
-    // Modify the file loader rule to ignore *.svg, since we have it handled now.
     fileLoaderRule.exclude = /\.svg$/i;
-
-    // Set up path alias
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
       '@': path.resolve(__dirname, 'src'),
     };
-    config.resolve.extensions = ['.ts', '.js'];
     return config;
   },
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ function page() {
   return (
     <div className="h-[50vh] bg-black text-white">
       home page hello cpanel again! let me do my try!! page page tailwind added to dep
+      <div className="bg-red-200">asghar help me!</div>
     </div>
   );
 }
