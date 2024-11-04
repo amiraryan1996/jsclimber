a1827e30980a4648f8faf2b2394fe0d0e7a1577a
amiraryan1996 <ariyaan021@gmail.com>
2024-11-04 18:30:52 +0330

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function page() {
   return (
-    <div className="h-[50vh] bg-gray-400">
+    <div className="h-[50vh] bg-blue-400">
       home page pm2 logs flushed github webhooks api updated server just runs pm2 stop then ignores
       other chained commands
     </div>
