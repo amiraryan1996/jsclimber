8ce86ec5b6a8c75a77540409cb3cbf341d114620
amiraryan1996 <ariyaan021@gmail.com>
2024-11-04 16:55:56 +0330

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function page() {
-  return <div className="h-[50vh] bg-green-400">home page github webhooks api updated</div>;
+  return <div className="h-[50vh] bg-gray-400">home page github webhooks api updated</div>;
 }
 
 export default page;
