f0090e19d225083cfcae279af30ece8ad65bcb55
amiraryan1996 <ariyaan021@gmail.com>
2024-11-04 23:52:55 +0330

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ function page() {
     <div className="h-[50vh] bg-black text-white">
       home page hello cpanel again! let me do my try!! page page tailwind added to dep
       <div className="bg-red-200">asghar help me! i test it</div>
-      <div>i will come back in 1 hour bye!</div>
+      <div>i will come back in 1 hour bye! i need sleep feeling asleepy</div>
     </div>
   );
 }
