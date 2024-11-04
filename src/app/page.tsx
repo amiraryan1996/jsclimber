969fb47900ba423d65d76022e2a7022fa4b8ff3d
amiraryan1996 <ariyaan021@gmail.com>
2024-11-05 01:16:12 +0330

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function page() {
-  return <div className="h-[50vh] bg-red-800 text-white">home page asghar dorst shod? </div>;
+  return (
+    <div className="h-[50vh] bg-red-800 text-white">
+      home page asgkjglllllllllllllllhar dorst shod?{' '}
+    </div>
+  );
 }
 
 export default page;
