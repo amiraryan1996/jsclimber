28b86e2bef849b3b1384f40872cb241a979e0559
amiraryan1996 <ariyaan021@gmail.com>
2024-11-04 22:46:04 +0330

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function page() {
   return (
-    <div className="h-[50vh] bg-yellow-800">home page hello cpanel again! let me do my try</div>
+    <div className="h-[50vh] bg-yellow-800">home page hello cpanel again! let me do my try!!</div>
   );
 }
 
