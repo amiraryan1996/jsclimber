6ecc973b4300282e627dfb39fbfe7527da2edf57
amiraryan1996 <ariyaan021@gmail.com>
2024-11-04 21:21:32 +0330

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function page() {
   return (
     <div className="h-[50vh] bg-yellow-800">
-      home page hello cpanel by deploy shell wee succeeded! we will do it?
+      home page hello cpanel by deploy shell wee succeeded! we will do it? I hope so ...{' '}
     </div>
   );
 }
