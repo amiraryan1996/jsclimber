d6f1fddf5dbe271fed5c6c386665e758a8923732
amiraryan1996 <ariyaan021@gmail.com>
2024-11-04 19:03:12 +0330

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function page() {
-  return <div className="h-[50vh] bg-blue-400">home page</div>;
+  return <div className="h-[50vh] bg-yellow-400">home page</div>;
 }
 
 export default page;
