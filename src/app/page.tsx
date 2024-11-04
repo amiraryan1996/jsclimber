aae0d262d2b46f8f0d597d11cc5c9f00571ae51d
amiraryan1996 <ariyaan021@gmail.com>
2024-11-04 22:58:18 +0330

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function page() {
   return (
     <div className="h-[50vh] bg-black text-white">
-      home page hello cpanel again! let me do my try!!
+      home page hello cpanel again! let me do my try!! page page tailwind added to dep
     </div>
   );
 }
