f4297a0e688fc721860d433bad158dfbee3881b3
amiraryan1996 <ariyaan021@gmail.com>
2024-11-04 18:23:21 +0330

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 function page() {
   return (
     <div className="h-[50vh] bg-gray-400">
-      home page pm2 logs flushed github webhooks api updated
+      home page pm2 logs flushed github webhooks api updated server just runs pm2 stop then ignores
+      other chained commands
     </div>
   );
 }
