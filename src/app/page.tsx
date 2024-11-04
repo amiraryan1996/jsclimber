86153fdc3d7830a5d30ee5e816d1164d1a2cda60
amiraryan1996 <ariyaan021@gmail.com>
2024-11-04 22:41:56 +0330

    hotfixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 function page() {
-  return <div className="h-[50vh] bg-yellow-800">home page hello cpanel again!</div>;
+  return (
+    <div className="h-[50vh] bg-yellow-800">home page hello cpanel again! let me do my try</div>
+  );
 }
 
 export default page;
