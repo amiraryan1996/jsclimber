211998b309c117a1e9f6c980029f5a21c7e8ed22
amiraryan1996 <ariyaan021@gmail.com>
2024-11-05 22:59:08 +0330

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function page() {
   return (
     <div className="h-[10vh]] border bg-red-400 text-white">
-      time:19:04 <h1 className="bg-gray-100">good night disk usage </h1>
+      time:19:04 <h1 className="bg-yellow-100 text-3xl">good night disk usage </h1>
     </div>
   );
 }
