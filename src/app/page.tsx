4a5bdae59812f9272b8ec6a36675aa795e09a967
amiraryan1996 <ariyaan021@gmail.com>
2024-11-05 15:15:13 +0330

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 function page() {
   return (
     <div className="h-[10vh]] border bg-red-400 text-white">
-      home page asgkjglllllllllllllllhar dorst shod? <h1>good night</h1>
+      today is 5 NOV 2024
+      <h1 className="bg-green-100">good morining</h1>
     </div>
   );
 }
