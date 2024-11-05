abef7f67dd0ac6c337eb3a665e65b072e7e05cc9
amiraryan1996 <ariyaan021@gmail.com>
2024-11-05 23:39:38 +0330

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 function page() {
   return (
     <div className="h-[10vh]] border bg-red-400 text-white">
-      time:19:04 <h1 className="bg-green-100 text-3xl">good night disk usage </h1>
       <p>shell script changed !</p>
     </div>
   );
