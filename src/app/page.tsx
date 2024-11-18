758b6f7e8737c6b2e8e2da20640e8d6f81e0e520
amiraryan1996 <amir.aryan.dv@gmail.com>
2024-11-18 16:36:14 +0330

    test(ci)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function page() {
   return (
-    <div className="h-[10vh] border bg-green-400 text-white">
+    <div className="h-[10vh] border bg-black text-white">
       <p>
         I&apos;m going to review storage of browser docs and its usage with context, zustand and all
         staff about client state management... .
