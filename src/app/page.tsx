c7841065474db5c2b78cbd1fd27f2ddf54a14aa4
amiraryan1996 <amir.aryan.dv@gmail.com>
2024-11-18 16:19:15 +0330

    ci test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ function page() {
     <div className="h-[10vh] border bg-green-400 text-white">
       <p>
         I&apos;m going to review storage of browser docs and its usage with context, zustand and all
-        staff about client state management.
+        staff about client state management... .
       </p>
     </div>
   );
