3fbfb3dccb7ac8fadaa85ee2be327d624e67946f
amiraryan1996 <amir.aryan.dv@gmail.com>
2024-11-18 16:17:06 +0330

    ci test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 function page() {
   return (
-    <div className="h-[10vh]] border bg-green-400 text-white">
-      <p>auth refactored !</p>
+    <div className="h-[10vh] border bg-green-400 text-white">
+      <p>
+        I&apos;m going to review storage of browser docs and its usage with context, zustand and all
+        staff about client state management.
+      </p>
     </div>
   );
 }
