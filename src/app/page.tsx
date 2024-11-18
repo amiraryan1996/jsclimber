019ea1830abdd4d019235b4c6b367362b38183e5
amiraryan1996 <amir.aryan.dv@gmail.com>
2024-11-18 15:33:04 +0330

    🐎 ci(crypto): github oauth app
    ci try, oauth url updated from localhost to jsclimber.ir

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function page() {
   return (
-    <div className="h-[10vh]] border bg-red-400 text-white">
-      <p>shell script changed !</p>
+    <div className="h-[10vh]] border bg-green-400 text-white">
+      <p>auth completed !</p>
     </div>
   );
 }
